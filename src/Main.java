0010225c9ee0abb1970f6c70bc4543301fcdb6f9
Eric Abell <ericabell@users.noreply.github.com>
2017-09-13 14:34:28 -0500

    Cleaned up the code a bit - including hashCode

--- src/Main.java
+++ src/Main.java
@@ -19,5 +19,23 @@ public class Main {
         } else {
             System.out.println("Clone is not equal to original");
         }
+
+        ((EmailNotification)emailNotificationClone).setStatus("new updated different status");
+
+        if( emailNotification1.equals(emailNotificationClone) ) {
+            System.out.println("Clone is equal to original");
+        } else {
+            System.out.println("Clone is not equal to original");
+        }
+
+        EmailNotification en1 = new EmailNotification("subject", "body", "recipient", "smtpProvider1");
+        EmailNotification en2 = new EmailNotification("subject", "body", "recipient", "smtpProvider");
+
+        if( en1.equals(en2) ) {
+            System.out.println("Clone is equal to original");
+        } else {
+            System.out.println("Clone is not equal to original");
+        }
+
     }
 }
